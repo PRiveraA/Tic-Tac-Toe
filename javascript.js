5ee9bd71538eb16a041ab7db107d53155b5df581
Pedro Rivera <priveraa94@gmail.com>
2025-08-07 17:52:28 -0400

    Adds win condition logic

--- javascript.js
+++ javascript.js
@@ -24,21 +24,43 @@ const game = function () {
     currentPlayer === player1 ? currentPlayer = player2 : currentPlayer = player1
   }
 
+  const checkWinner = function (board, marker) {
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+    // Checks that in any of the combinations all of the indexes are the same as the marker
+    return winningCombinations.some(combination => {
+      return combination.every(index => board[index] === marker);
+    })
+  }
+
+  const declareWinner = function (currentPlayer) {
+    console.log(`${currentPlayer.name} is the winner`);
+  }
+
   const play = function (choice) {
 
     if (Gameboard.getBoard()[choice] === "") {
       Gameboard.markSquare(choice, currentPlayer.symbol)
       turnCount = turnCount + 0.5
-      turnAlternator()
-      console.log(`It is now ${currentPlayer.name}'s turn`);
-      console.log(Gameboard.getBoard())
     } else { console.log("This square has already been selected, choose a different square."); }
 
     if (turnCount > 2.5) {
       console.log("Checking wincon");
+      if (checkWinner(Gameboard.getBoard(), currentPlayer.symbol) === true) {
+        declareWinner(currentPlayer)
+      }
     }
+    turnAlternator()
+    console.log(`It is now ${currentPlayer.name}'s turn`);
   }
 
   return { play }
 }
-
