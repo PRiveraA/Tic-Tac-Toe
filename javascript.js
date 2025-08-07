12259375adf7fdd5fdc96d7e3b5cb59a09fd7092
Pedro Rivera <priveraa94@gmail.com>
2025-08-07 16:04:24 -0400

    Creates most of the game logic

--- javascript.js
+++ javascript.js
@@ -3,14 +3,42 @@ const Gameboard = (() => {
   const getBoard = () => board
   const resetBoard = () => board = ["", "", "", "", "", "", "", "", ""]
   const markSquare = function (index, symbol) {
-    if (board[index] === "") {
-      board[index] = symbol
-    } else { console.log("Square already marked, choose a different square"); }
+    board[index] = symbol
   }
   return { getBoard, resetBoard, markSquare }
 })()
 
 const Player = function (name, symbol) {
-
   return { name, symbol }
 }
+
+const player1 = Player("Pedro", "X")
+const player2 = Player("Kim", "O")
+
+let currentPlayer = player1
+let turnCount = 1
+
+const game = function () {
+
+  const turnAlternator = function () {
+    currentPlayer === player1 ? currentPlayer = player2 : currentPlayer = player1
+  }
+
+  const play = function (choice) {
+
+    if (Gameboard.getBoard()[choice] === "") {
+      Gameboard.markSquare(choice, currentPlayer.symbol)
+      turnCount = turnCount + 0.5
+      turnAlternator()
+      console.log(`It is now ${currentPlayer.name}'s turn`);
+      console.log(Gameboard.getBoard())
+    } else { console.log("This square has already been selected, choose a different square."); }
+
+    if (turnCount > 2.5) {
+      console.log("Checking wincon");
+    }
+  }
+
+  return { play }
+}
+
